1b2d054e6e2493bf31a397ef08741b964fc7425e
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 14:25:49 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,7 +2,6 @@ class RepositoriesController < ApplicationController
   def index
     resp = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
     @repos = JSON.parse(resp.body)
-    binding.pry
   end
 
   def create
