c3e39688764926f16943046d96e7b80ac38f34ad
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 14:41:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user
+  ID = ''
 
   private
 
     def authenticate_user
-      client_id = 'b2c1d48b8d0cedb3e062'
-      #redirect_uri = CGI.escape("http://165.227.31.208:42405/auth")
-      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"#&redirect_uri=#{redirect_uri}"
+      client_id = ENV['CLIENT_ID']
+      github_url = "https://github.com/login/oauth/authorize?client_id=#{client_id}"
       redirect_to github_url unless logged_in?
     end
 
