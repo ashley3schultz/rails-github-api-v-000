33094de38738452001504e15bc5598f8b6d4396d
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 14:16:14 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,11 +10,7 @@ class SessionsController < ApplicationController
         'Accept' => 'application/json'})
     session[:token] = JSON.parse(resp.body)["access_token"]
     user_response = Faraday.get("https://api.github.com/user", {}, {
-<<<<<<< HEAD
       'Authorization' => "token #{session[:token]}",
-=======
-      'Authorization' => "token #{params[:code]}",
->>>>>>> e62e717b6f50f76a5736999658ef95404769a546
       'Accept' => 'application/json'})
     session[:username] = JSON.parse(user_response.body)["login"]
 
