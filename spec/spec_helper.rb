595bb5d3ab9e6abd22d3046fd489a52459c48a79
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 14:38:12 +0000

    automatically backed up by learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,7 +17,7 @@ RSpec.configure do |config|
       to_return(:status => 200, :body => [{"name" => "Repo 1", "html_url" => "http://link1.com"}, {"name" => "Repo 2", "html_url" => "http://link2.com"}, {"name" => "Repo 3", "html_url" => "http://link3.com"}].to_json, :headers => {})
 
     stub_request(:post, "https://github.com/login/oauth/access_token").
-      with(:body => {"client_id"=> b2c1d48b8d0cedb3e062, "client_secret"=> bd32ab4044c68851b047adde1bbb8d5c342bfa9f, "code"=>"20"},
+      with(:body => {"client_id"=> 'b2c1d48b8d0cedb3e062', "client_secret"=> 'bd32ab4044c68851b047adde1bbb8d5c342bfa9f', "code"=>"20"},
       :headers => {'Accept'=>'application/json'}).
       to_return(:status => 200, :body => {"access_token"=>"1"}.to_json, :headers => {})
 
