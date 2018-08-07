86a0c93b13880dcf2a8c1051c37e2de0c4c95e9f
Ashley Schultz <ashleyeschultz@gmail.com>
2018-08-07 14:42:58 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/auth' => 'sessions#create'
   post '/repositories/create' => 'repositories#create'
   root 'repositories#index'
-  get '/auth?code=20' => 'repositories#other'
+  get '/login/oauth/authorize' => 'repositories#other'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
